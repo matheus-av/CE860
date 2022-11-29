e197729e79c9a776b053d1452e1f62e921fa1598
renan-pechutti <111813775+renan-pechutti@users.noreply.github.com>
2022-11-29 12:17:31 -0300

    Create bancoProdutos.py

---
+++ bancoProdutos.py
@@ -0,0 +1,49 @@
+import sqlite3
+
+def criarTabelaProdutos():
+  connection = sqlite3.connect('banco.db')
+  cursor = connection.cursor()
+  sqlProdutos = '''
+    create table Produtos(
+      sku integer not null primary key,
+      nome text not null,
+      preco real not null,
+      custo real not null
+    )'''
+  cursor.execute(sqlProdutos)
+  connection.commit()
+  cursor.close()
+  connection.close()
+
+def cadastrarProduto(sku: int, nome: str, preco: float, custo: float):
+  connection = sqlite3.connect('banco.db')
+  cursor = connection.cursor()
+  cursor.execute(f"insert into Produtos (sku, nome, preco, custo) values ('{sku}', '{nome}', '{preco}', '{custo}')")
+  connection.commit()
+  cursor.close()
+  connection.close()
+
+def consultarProdutos():
+  connection = sqlite3.connect('banco.db')
+  cursor = connection.cursor()
+  cursor.execute('select * from Produtos')
+  todosProdutos = cursor.fetchall()
+  cursor.close()
+  connection.close()
+  return todosProdutos
+
+def atualizarProduto(sku: int, nome: str, preco: float, custo: float):
+  connection = sqlite3.connect('banco.db')
+  cursor = connection.cursor()
+  cursor.execute(f"update Produtos set nome = '{nome}', preco = '{preco}', custo = '{custo}' where sku = '{sku}'")
+  connection.commit()
+  cursor.close()
+  connection.close()
+
+def excluirProduto(sku: str):
+  connection = sqlite3.connect('banco.db')
+  cursor = connection.cursor()
+  cursor.execute(f"delete from Produtos where sku = '{sku}'")
+  connection.commit()
+  cursor.close()
+  connection.close()
