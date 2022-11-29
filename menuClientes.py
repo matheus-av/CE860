e675d6394ab07b32913fda234c0f2a90a84ecaf6
matheus-av <97976022+matheus-av@users.noreply.github.com>
2022-11-29 11:11:15 -0300

    Add files via upload

---
+++ menuClientes.py
@@ -0,0 +1,72 @@
+import validacoes
+import bancoClientes
+
+def menuClientes():
+    print('1 - Cadastrar Cliente')
+    print('2 - Consultar Clientes')
+    print('3 - Atualizar Cliente')
+    print('4 - Excluír Cliente')
+    print('0 - Sair')
+
+    opcao = input('Digite o número de uma opção:')
+
+    if opcao == 1:
+        cadastrarCliente()
+    elif opcao == 2:
+        consultarClientes()
+    elif opcao == 3:
+        atualizarCliente()
+    elif opcao == 4:
+        excluirCliente()
+    elif opcao != 0:
+        print('Opção inválida')
+
+def cadastrarCliente():
+    cpf = input('Digite o CPF do cliente:')
+    while not validacoes.cpfValido(cpf):
+        print('CPF inválido')
+        cpf = input('Digite o CPF do cliente:')
+
+    nome = input('Digite o NOME do cliente:')
+    while not validacoes.nomeValido(nome):
+        print('NOME inválido')
+        nome = input('Digite o NOME do cliente:')
+    
+    estado = input('Digite o ESTADO do cliente:')
+    while not validacoes.estadoValido(estado):
+        print('ESTADO inválido')
+        estado = input('Digite o ESTADO do cliente:')
+
+    bancoClientes.cadastrarCliente(cpf, nome, estado)
+    print('Cliente Cadastrado')
+
+def consultarClientes():
+    print(bancoClientes.consultarClientes())
+
+def atualizarCliente():
+    cpf = input('Digite o CPF do cliente:')
+    while not validacoes.cpfValido(cpf):
+        print('CPF inválido')
+        cpf = input('Digite o CPF do cliente:')
+
+    nome = input('Digite o NOME do cliente:')
+    while not validacoes.nomeValido(nome):
+        print('NOME inválido')
+        nome = input('Digite o NOME do cliente:')
+    
+    estado = input('Digite o ESTADO do cliente:')
+    while not validacoes.estadoValido(estado):
+        print('ESTADO inválido')
+        estado = input('Digite o ESTADO do cliente:')
+
+    bancoClientes.atualizarCliente(cpf, nome, estado)
+    print('Cliente Atualizado')
+
+def excluirCliente():
+    cpf = input('Digite o CPF do cliente:')
+    while not validacoes.cpfValido(cpf):
+        print('CPF inválido')
+        cpf = input('Digite o CPF do cliente:')
+
+    bancoClientes.excluirCliente(cpf)
+    print('Cliente Excluído')
