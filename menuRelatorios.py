abbcef8ee03b8b6a25b080bf1ef6c306fa936718
matheus-av <97976022+matheus-av@users.noreply.github.com>
2022-11-29 12:01:07 -0300

    Add files via upload

---
+++ menuRelatorios.py
@@ -0,0 +1,34 @@
+import validacoes
+import bancoRelatorios
+
+def menuRelatorios():
+    print('1 - Receita total')
+    print('2 - Vendas por Localidade')
+    print('3 - Produtos mais vendidos')
+    print('4 - Lucros e custos totais')
+    print('0 - Sair')
+
+    opcao = input('Digite o número de uma opção:')
+
+    if opcao == 1:
+        relatorioReceitaTotal()
+    elif opcao == 2:
+        vendasPorLocalidade()
+    elif opcao == 3:
+        produtosMaisVendidos()
+    elif opcao == 4:
+        lucrosCustosTotais()
+    elif opcao != 0:
+        print('Opção inválida')
+
+def relatorioReceitaTotal():
+    return 0 #FAZER
+
+def vendasPorLocalidade():
+    return 0 #FAZER
+
+def produtosMaisVendidos():
+    return 0 #FAZER
+
+def lucrosCustosTotais():
+    return 0 #FAZER
