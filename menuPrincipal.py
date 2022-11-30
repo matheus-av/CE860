07d2623750d9d10cbcd4049910bef6abd74263ff
renan-pechutti <111813775+renan-pechutti@users.noreply.github.com>
2022-11-30 11:00:04 -0300

    Update menuPrincipal.py

--- menuPrincipal.py
+++ menuPrincipal.py
@@ -10,21 +10,19 @@ def menuPrincipal():
         print('3 - Vendas')
         print('4 - Relatórios')
         print('0 - Sair')
-
+        
         opcao = input('Digite o número de uma opção:')
         
-
         if opcao == "1":
-            menuClientes()
+            menuClientes.menuClientes()
         elif opcao == "2":
-            menuProdutos()
+            menuProdutos.menuProdutos()
         elif opcao == "3":
-            menuVendas()
+            menuVendas.menuVendas()
         elif opcao == "4":
-            menuRelatorios()
+            menuRelatorios.menuRelatorios()
         elif opcao == "0":
             print('Programa finalizado')
             break
         else:
             print('Opção inválida')
-            
